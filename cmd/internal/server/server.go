f08142cf2e661e963d852c3627a202f871bb9e7c
Andrey Kofanov <a14449551@gmail.com>
2025-07-19 15:26:55 +0300

    Server start and shutdown

--- cmd/internal/server/server.go
+++ cmd/internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"github.com/labstack/echo/v4"
 	"log/slog"
 )
@@ -15,4 +16,14 @@ func New(log *slog.Logger) *Server {
 	serverEcho := echo.New()
 
 	return &Server{serverEcho, ":8080", log}
+}
+
+func (server *Server) Start() error {
+	server.log.Info("Starting server", "addr", server.addr)
+	return server.echo.Start(server.addr)
+}
+
+func (server *Server) Shutdown(ctx context.Context) error {
+	server.log.Info("Shutting down server")
+	return server.echo.Shutdown(ctx)
 }
