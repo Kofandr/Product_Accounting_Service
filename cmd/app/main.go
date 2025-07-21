1fd420483117d76234c936a4a9935158479a1985
Andrey Kofanov <a14449551@gmail.com>
2025-07-21 16:07:34 +0300

    Config++

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"context"
+	"github.com/Kofandr/Product_Accounting_Service/internal/config"
+	"github.com/Kofandr/Product_Accounting_Service/internal/logger"
+	"github.com/Kofandr/Product_Accounting_Service/internal/server"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
+	cfg := config.Mustload()
+	mainLog := logger.New(cfg.LoggerLevel)
+
+	mainServer := server.New(mainLog)
+
+	go func() {
+		if err := mainServer.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server crash")
+
+		}
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	<-signalChan
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	mainLog.Info("Shutting down...")
+	if err := mainServer.Shutdown(ctx); err != nil {
+		mainLog.Error("Shutdown failed", "error", err)
+	} else {
+		mainLog.Info("Server stopped")
+	}
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,42 @@
 package config
 
 import (
+	"fmt"
 	"github.com/caarlos0/env/v8"
+	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"log"
 )
 
 type Configuration struct {
-	Port        string `yaml:"Port" env:"PORT" envdefault:"8080"`
-	LoggerLevel string `yaml:"LoggerLevel" env:"LOGGER_LEVEL" envdefault:"INFO"`
+	Port        int    `env:"PORT" envdefault:"8080" validate:"required,min=1,max=65535"`
+	LoggerLevel string `env:"LOGGER_LEVEL" envdefault:"INFO" validate:"required,oneof=DEBUG INFO WARN ERROR"`
 }
 
-func Load() *Configuration {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading Config.env file: %s", err)
-	}
+func Load() (*Configuration, error) {
 	cfg := &Configuration{}
 
-	if err = env.Parse(cfg); err != nil {
-		log.Fatalf("Failed to parse environment variables: %s", err)
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("Error loading Config.env file: %s", err)
+	}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("Failed to parse environment variables: %s", err)
+	}
+
+	if err := validator.New().Struct(cfg); err != nil {
+		return nil, fmt.Errorf("validator error: %s", err)
+	}
+
+	return cfg, nil
+
+}
+
+func Mustload() *Configuration {
+	cfg, err := Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
 	}
 	return cfg
-
 }
