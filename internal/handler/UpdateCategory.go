0a44e8675e1cfb25248552c02075e5b852b6d49e
Andrey Kofanov <a14449551@gmail.com>
2025-07-30 20:09:52 +0300

    taskOne: add UpdateCategory handler and unit tests

--- internal/handler/UpdateCategory.go
+++ internal/handler/UpdateCategory.go
@@ -1,7 +1,43 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/Kofandr/Product_Accounting_Service/internal/logger"
+	"github.com/Kofandr/Product_Accounting_Service/internal/model"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+)
 
 func (handler *Handler) UpdateCategory(c echo.Context) error {
-	return nil
+	logg := logger.MustLoggerFromCtx(c.Request().Context())
+
+	ctx := c.Request().Context()
+
+	stringId := c.Param("id")
+
+	categoryId, err := strconv.Atoi(stringId)
+	if err != nil {
+		errResp := map[string]string{"err": "Invalid id"}
+		logg.Info("Invalid id", "err", err)
+		return c.JSON(http.StatusBadRequest, errResp)
+	}
+
+	var category model.UpdateCategoryRequest
+	if err := c.Bind(&category); err != nil {
+		errResp := map[string]string{"err": "Invalid JSON format"}
+		logg.Error("Invalid JSON received")
+		return c.JSON(http.StatusBadRequest, errResp)
+	}
+
+	err = handler.db.UpdateCategory(ctx, categoryId, &category)
+	if err != nil {
+		errResp := map[string]string{"err": "Server error"}
+		logg.Error("An error occurred while accessing the database", "err", err)
+		return c.JSON(http.StatusInternalServerError, errResp)
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"Request Status": "Changes completed",
+	})
+
 }
--- internal/handler/UpdateCategory_test.go
+++ internal/handler/UpdateCategory_test.go
@@ -1,7 +1,14 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/Kofandr/Product_Accounting_Service/internal/repository/mocks"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -24,10 +31,47 @@ func TestUpdateCategory(t *testing.T) {
 			expectedStatus: http.StatusOK,
 			expectedBody:   `{"Request Status": "Changes completed"}`,
 		},
+		{
+			name:           "Invalid id",
+			param:          "abc",
+			mockOn:         1,
+			mockReturn:     nil,
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   `{"err": "Invalid id"}`,
+		},
+		{
+			name:           "not found",
+			param:          "999",
+			mockOn:         999,
+			mockReturn:     errN,
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   `{"err": "Not found"}`,
+		},
+		{
+			name:           "Server error",
+			param:          "1",
+			mockOn:         1,
+			mockReturn:     fmt.Errorf("erro"),
+			expectedStatus: http.StatusInternalServerError,
+			expectedBody:   `{"err": "Server error"}`,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			
+			mockBD := new(mocks.Repository)
+			c := echo.New()
+			req := httptest.NewRequest(http.MethodPost, "/", nil)
+			rec := httptest.NewRecorder()
+			cT := c.NewContext(req, rec)
+			cT.SetParamNames("id")
+			cT.SetParamValues(test.param)
+			mockBD.On("UpdateCategory", mock.Anything, test.mockOn, mock.Anything).Return(test.mockReturn, test.mockReturn)
+			handler := New(mockBD)
+			handler.UpdateCategory(cT)
+
+			assert.Equal(t, test.expectedStatus, rec.Code)
+
+			assert.JSONEq(t, test.expectedBody, strings.TrimSpace(rec.Body.String()))
 		})
 	}
 }
